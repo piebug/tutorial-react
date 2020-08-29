5d8b48d44c3753d42d2178b1c6bae9faa03c879a
sandy <amwright.dev@gmail.com>
2020-08-29 17:14:23 -0500

    Updates

---
+++ precache-manifest.12645690ec6d513cab0defdb710cf1bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d20a2f7e83a0169dadcc87cddb438b",
+    "url": "/tutorial-react/index.html"
+  },
+  {
+    "revision": "e28fa8030d43f39b00dc",
+    "url": "/tutorial-react/static/css/main.52db46fd.chunk.css"
+  },
+  {
+    "revision": "0c288c5cbe5aa91590f9",
+    "url": "/tutorial-react/static/js/2.eb3c6d82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tutorial-react/static/js/2.eb3c6d82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e28fa8030d43f39b00dc",
+    "url": "/tutorial-react/static/js/main.b567be3a.chunk.js"
+  },
+  {
+    "revision": "682edddca3f6012c4544",
+    "url": "/tutorial-react/static/js/runtime-main.4b9bd8d2.js"
+  }
+]);
