832e194555a4ba1c0fc823cd014e5abba0b7450e
sandy <amwright.dev@gmail.com>
2020-08-30 20:51:45 -0500

    Break out and reorganize index components

--- src/index.js
+++ src/index.js
@@ -7,6 +7,25 @@ import tictac from './static/tic-tac.svg';
 import tictactoe from './static/tic-tac-toe.svg';
 import './index.css';
 
+function ReactLogo(props) {
+  return (
+    <div className="text-center">
+      <img src={logo} className="logo" alt="ReactJS logo" />
+      <p>
+        <a href="https://reactjs.org/" className="react-home">React</a>
+      </p>
+    </div>
+  );
+}
+
+function PillToeIcons(props) {
+  return (
+    <div>
+      <img src={tictac} className="tic-tac" alt="tic-tac" />
+      <img src={toe} className="toe" alt="toe" />
+    </div>
+  );
+}
 
 function IconsAttribute(props) {
   return (
@@ -20,26 +39,28 @@ function IconsAttribute(props) {
   );
 }
 
+function MadeBy(props) {
+  return (
+    <h2 className="made-by mb-0 pb-1">
+      Made by Sandy.
+    </h2>
+  );
+}
+
 function Footer(props) {
   return (
     <footer className="footer px-md-5 px-3 py-3">
       <div className="container-fluid px-lg-5">
         <div className="d-md-flex justify-content-between align-items-top mt-4">
           <div>
-            <p className="made-by mb-1">
-              Made by Sandy.
-            </p>
+            <MadeBy />
             <IconsAttribute />
-            <div class="text-center text-md-left">
-              <img src={tictac} className="tic-tac" alt="tic-tac" />
-              <img src={toe} className="toe" alt="toe" />
+            <div className="text-center text-md-left">
+              <PillToeIcons />
             </div>
           </div>
-          <div className="text-center mt-3 mt-md-0">
-            <img src={logo} className="logo" alt="ReactJS logo" />
-            <p>
-              <a href="https://reactjs.org/" className="react-home">React</a>
-            </p>
+          <div className="mt-3 mt-md-0">
+            <ReactLogo />
           </div>
         </div>
       </div>
@@ -47,16 +68,29 @@ function Footer(props) {
   );
 }
 
+function NavbarIcon(props) {
+  return (
+    <a
+      className="navbar-brand"
+      href="https://github.com/simply-so/tutorial-react"
+    >
+      <img
+        src={tictactoe}
+        className="tic-tac-toe d-inline-block align-center mr-2"
+        alt="tic-tac-toe"
+      />
+      <span className="d-none d-md-inline">
+        : a <span className="react-js">reactjs</span> practice app
+      </span>
+    </a>
+  );
+}
+
 function Header(props) {
   return (
     <header className="header px-md-5 px-3 py-2">
       <nav className="navbar navbar-light px-lg-5">
-        <a className="navbar-brand" href="https://github.com/simply-so/tutorial-react">
-          <img src={tictactoe} className="tic-tac-toe d-inline-block align-center mr-2" alt="tic-tac-toe" />
-          <span className="d-none d-md-inline">
-            : a <span className="react-js">reactjs</span> practice app
-          </span>
-        </a>
+        <NavbarIcon />
       </nav>
     </header>
   );
